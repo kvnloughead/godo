9a7413f2086ec20b421d46354e044004d37dc5c7
Kevin Loughead <kvnloughead@gmail.com>
2024-05-27 12:29:20 -0400

    feat: allow A..Z for priority_level

--- migrations/000001_create_todos_table.up.sql
+++ migrations/000001_create_todos_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TYPE priority_level AS ENUM ('A', 'B', 'C');
+CREATE TYPE priority_level AS ENUM ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 
 CREATE TABLE IF NOT EXISTS todos (
   id bigserial PRIMARY KEY,
